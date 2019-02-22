cb769bfe534fabe88ee1bf6f89e52e255b062a11
GeGe-K <gloriagivondo@gmail.com>
2019-02-22 18:24:02 +0300

    added the js functionality

---
+++ scripts.js
@@ -0,0 +1,19 @@
+// window.onload = function(){
+//     document.getElementById('start').style.dasplay='none';
+//     function showDomElements(){
+//         document.getElementById('start').style.display='block'
+//     };
+
+// };
+// function showTags(){
+//     var c = document.getElementById('start').childNodes;
+//     var txt = "";
+//     var i;
+//     for (i = 0; i < c.length; i++){
+//         txt = txt + c[i].nodeName + ",";
+//     }
+//     document.getElementById("display").innerHTML = txt;
+// };
+// function showDomElements(){
+//     document.getElementById('start').style.display = 'block'
+// };
